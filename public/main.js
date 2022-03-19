3e0e12e1745515070ad24c0607d8e0d5b0de55b5
Script Raccoon <scriptraccoon@gmail.com>
2022-03-20 00:31:24 +0100

    folder structure

--- public/main.js
+++ public/main.js
@@ -1,6 +1,6 @@
-import {Population} from "./population.js";
-import {start,goal} from "./point.js";
-import {Rectangle} from "./rectangle.js";
+import { Population } from "./population.js";
+import { start, goal } from "./point.js";
+import { Rectangle } from "./rectangle.js";
 export let pointAlpha = 0.5;
 
 // CANVAS
@@ -10,30 +10,28 @@ ctx.lineWidth = 1;
 ctx.strokeStyle = "black";
 
 // POPULATION
-let population = new Population(500,1000);
+let population = new Population(500, 1000);
 population.initialize();
 population.draw(ctx);
 
 // OBSTACLES
 let obstacles = [];
-obstacles.push(new Rectangle([400,400],[500,30],[0,0]));
-
+obstacles.push(new Rectangle([400, 400], [500, 30], [0, 0]));
 
 // LOOP
 let step = 0;
 loop();
 
 function loop() {
-
     // UPDATE
-    obstacles.forEach(obstacle => obstacle.update(ctx));
-    population.update(ctx,step,obstacles);
-     
+    obstacles.forEach((obstacle) => obstacle.update(ctx));
+    population.update(ctx, step, obstacles);
+
     // DRAW
-    ctx.clearRect(0,0,ctx.canvas.width,ctx.canvas.height);
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
     start.draw(ctx);
-    goal.draw(ctx);    
-    obstacles.forEach(obstacle => obstacle.draw(ctx));
+    goal.draw(ctx);
+    obstacles.forEach((obstacle) => obstacle.draw(ctx));
     population.draw(ctx);
 
     step++;
@@ -41,7 +39,7 @@ function loop() {
     // REPRODUCTION
     if (step >= population.lifeSpan || population.done) {
         population.reproduce();
-        obstacles.forEach(obstacle => obstacle.restart(ctx));
+        obstacles.forEach((obstacle) => obstacle.restart(ctx));
         step = 0;
     }
 
@@ -49,12 +47,8 @@ function loop() {
 }
 
 // HIDE POINTS BY KEY INPUT
-document.addEventListener("keydown",(e) => {
+document.addEventListener("keydown", (e) => {
     if (e.key == "o") {
         pointAlpha = 0.5 - pointAlpha;
     }
 });
-
-
-
-
